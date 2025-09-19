f1203abc47397ce53f9e2ebc23c19607d35ec9dd
piere05 <piereshc@gmail.com>
2025-09-19 16:21:37 +0530

    Cart page chnages

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:get/get.dart';
+//import 'package:get/get.dart';
 
 class CartPage extends StatefulWidget {
   @override
